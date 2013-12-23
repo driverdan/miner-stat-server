55ce9134b1366512db29a0a23f2d6b5c928cdf83
Dan DeFelippi <dan@driverdan.com>
2013-12-22 23:14:09 -0600

    Add StatSummary

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -10,13 +10,10 @@ socket.on('disconnect', function () {
   console.log('disconnected');
 });
 
+/**
+ * Main app
+ */
 var App = React.createClass({
-  render: function () {
-    return <Servers />;
-  }
-});
-
-var Servers = React.createClass({
   getInitialState: function () {
     socket.on('summary', function (data) {
       this.setState({servers: data});
@@ -25,7 +22,40 @@ var Servers = React.createClass({
     return {servers: []};
   },
   render: function () {
-    var servers = this.state.servers.map(function (server) {
+    return (
+      <div>
+        <StatSummary servers={this.state.servers} />
+        <Servers servers={this.state.servers} />
+      </div>
+    );
+  }
+});
+
+var StatSummary = React.createClass({
+  render: function () {
+    var curHashRate = 0, avgHashRate = 0;
+
+    this.props.servers.forEach(function (server) {
+      curHashRate += server.summary['MHS 5s'] * 1;
+      avgHashRate += server.summary['MHS av'] * 1;
+    });
+
+    return (
+      <div className="row">
+        <div className="small-6 columns">
+          <h2>{curHashRate.toFixed(2)} MH/s <small>{avgHashRate.toFixed(2)} MH/s avg</small></h2>
+        </div>
+      </div>
+    );
+  }
+});
+
+/**
+ * Servers list
+ */
+var Servers = React.createClass({
+  render: function () {
+    var servers = this.props.servers.map(function (server) {
       return <Server key={server.id} data={server.summary} />;
     });
 
@@ -37,20 +67,25 @@ var Servers = React.createClass({
   }
 });
 
+/**
+ * Individual server info
+ */
 var Server = React.createClass({
   render: function () {
     var data = this.props.data;
 
-    var summary = Object.keys(this.props.data).map(function (key) {
+    var summary = Object.keys(data).map(function (key) {
       return <li><strong>{key}:</strong> {' '} {data[key]}</li>;
     });
 
     return (
-      <div className="small-4 columns">
-        <h2>{this.props.key}</h2>
-        <ul>
-          {summary}
-        </ul>
+      <div className="small-12 medium-6 columns">
+        <div className="panel">
+          <h2>{this.props.key}</h2>
+          <ul>
+            {summary}
+          </ul>
+        </div>
       </div>
     );
   }
